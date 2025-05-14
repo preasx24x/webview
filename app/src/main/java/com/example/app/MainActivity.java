62dd5767bc6ec05d0ed3480c081c88623384d8bd
preasx24x <nakiaklocko57@gmail.com>
2025-05-14 12:09:11 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
         mWebView.setWebViewClient(new MyWebViewClient());
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+         mWebView.loadUrl("https://www.dtech-services.co.za");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
